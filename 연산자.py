b111e61944dcf9fa60deabfb64cca625d704000a
italianroses <112529060+italianroses@users.noreply.github.com>
2023-11-03 13:18:57 +0900

    Add files via upload

---
+++ 연산자.py
@@ -0,0 +1,35 @@
+print(1 + 1) # 2
+print(3 - 2) # 1
+print(5 * 2) # 10
+print(6 / 3) # 2.0
+
+
+print(2 ** 3) # 2^3 = 8
+print(5 % 3) # 5 나누기 3의 나머지
+print(5 // 3) # 5 나눈기 3의 몫
+print(10 // 3) # 10 나누기 3의 몫
+
+
+print(10 > 7) # True
+print(4 >= 7) # False
+print(10 < 3) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(3 != 3) # False
+print(4 + 7 == 11) # True
+
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True
+
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
+
