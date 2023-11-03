95b50aa1dfb51b385f846f78fd7faadf39870ee5
italianroses <112529060+italianroses@users.noreply.github.com>
2023-11-03 13:26:07 +0900

    Add files via upload

---
+++ 간단한 수식.py
@@ -0,0 +1,22 @@
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+numbers = 2 + 3 * 4 # 14
+print(numbers)
+
+numbers = numbers + 2
+print(numbers) # 16
+
+numbers += 2
+print(numbers) # 18
+
+numbers *= 2
+print(numbers) # 36
+
+numbers /= 2
+print(numbers) # 18
+
+numbers -= 2
+print(numbers) # 16
+
+numbers %= 5 # 1
+print(numbers)
