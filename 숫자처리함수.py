d5e14170ed0a68338f5669c3f15880dd1852db6a
italianroses <112529060+italianroses@users.noreply.github.com>
2023-11-03 13:33:32 +0900

    Add files via upload

---
+++ 숫자처리함수.py
@@ -0,0 +1,14 @@
+print(abs(-5)) # 5 abs : absolute 절댓값
+print(pow(4, 2)) # 4^2 = 4 * 4 = 16 제곱
+print(max(5, 12)) # 12 최댓값
+print(min(5, 12)) # 5 최솟값
+
+
+print(round(3.14)) # 3 반올림
+print(round(4.99)) # 5
+
+from math import * # math library 의 모든것을 불러온다
+
+print(floor(4.99)) # 4 내림
+print(ceil(3.14)) # 4 올림
+print(sqrt(16)) # 4 제곱근
