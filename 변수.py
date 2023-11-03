a2c9641625f836116f0d51003dad7f702d8029d0
italianroses <112529060+italianroses@users.noreply.github.com>
2023-11-03 09:47:57 +0900

    Add files via upload

---
+++ 변수.py
@@ -0,0 +1,24 @@
+# 애완동무릉ㄹ 소개해 주세요
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+
+print("우리집 "+ animal + "의 이름은 " + name + "예요")
+print(name + "는" + str(age) + "살이며" + hobby + "를 좋아해요") # 정수형을 문자형을 바꿀때는 str
+# print(name , "는" , age ,"살이며" ,hobby , "를 좋아해요") # 문자형 연결 , 로도 가능 이때는 str로 환산할 필요 없음 다만
+# , 자리에는 띄어쓰기 무조건 포함
+print(name + "는 어른일까요? " + str(is_adult)) # boolen도 문자형 str
+
+
+'''주석 처리 방법'''
+
+# 12
+# 12
+# 123
+# 132
+
+# 여러문장 선택후에 alt + / 처리하면 한꺼번에 주석처리 완료
