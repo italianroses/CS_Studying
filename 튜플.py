5daacd13004a306341e6a5ef2ee06b244dd9ac3b
italianroses <112529060+italianroses@users.noreply.github.com>
2023-11-14 10:23:04 +0900

    Add files via upload

---
+++ 튜플.py
@@ -0,0 +1,18 @@
+# 제한적인 사용성을 가지지만 list와 나머지 에 비해 속도가 빠르다.
+
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+
+# menu.add("생선까스") 튜플은 추가 불가능
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+
+name, age, hobby = "김종국", "20", "코딩"
+print(name, age, hobby)
+
+
