ad8b1995e8ef9d22e538675d85f5db192781eea7
italianroses <112529060+italianroses@users.noreply.github.com>
2023-12-03 17:04:19 +0900

    Add files via upload

--- Quiz. 5.py
+++ Quiz. 5.py
@@ -29,6 +29,21 @@ while index <= 50:
 print("총 탑승 승객 : {0} 분".format(count))
 
 
+ # Answer
+from random import *
+
+count = 0
+
+for index in range(1, 51):
+    time = randint(5, 50)
+    if 5 <= time <= 15:
+        print("[O] {0}번째 손님 (소요시간 : {1}분)".format(index, time))
+        count += 1
+    else:
+        print("[ ] {0}번째 손님 (소요시간 : {1}분)".format(index, time))
+
+print("총 탑승 승객 : {0} 분".format(count))
+
 
 
 
