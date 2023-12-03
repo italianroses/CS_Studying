26afd20d1c24904ede8a27d3f459ad46a4b1a9ba
italianroses <112529060+italianroses@users.noreply.github.com>
2023-12-03 16:12:43 +0900

    Add files via upload

---
+++ for.py
@@ -0,0 +1,15 @@
+for waiting_no in range(1, 6):
+    print(f"대기번호 : {waiting_no}")
+# 대기번호 : 1
+# 대기번호 : 2
+# 대기번호 : 3
+# 대기번호 : 4
+# 대기번호 : 5
+
+
+starbucks = ["아이언맨", "토르", "그루트"]
+for customer in starbucks:
+    print("{0}, 커피가 준비되었습니다.".format(customer))
+# 아이언맨, 커피가 준비되었습니다.
+# 토르, 커피가 준비되었습니다.
+# 그루트, 커피가 준비되었습니다.
